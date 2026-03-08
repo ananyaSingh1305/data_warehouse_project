1459d1076db165159ec231b98f94bc1bb1e9b7fc
Ananya Singh <88341482+ananyaSingh1305@users.noreply.github.com>
2026-03-08 16:20:17 +0530

    Create load_bronze.sql to load data into bronze tables
    This script truncates existing tables and loads new data from CSV files into the bronze layer of the data warehouse.

---
+++ scripts/bronze/load_bronze.sql
@@ -0,0 +1,52 @@
+TRUNCATE TABLE bronze_crm_cust_info;
+
+LOAD DATA LOCAL INFILE '/Users/ananyasingh/Documents/DataEngineering/DataWithBaraa/DataWarehouse/sql-data-warehouse-project-main/datasets/source_crm/cust_info.csv'
+INTO TABLE bronze_crm_cust_info
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+TRUNCATE TABLE bronze_crm_prod_info;
+
+LOAD DATA LOCAL INFILE '/Users/ananyasingh/Documents/DataEngineering/DataWithBaraa/DataWarehouse/sql-data-warehouse-project-main/datasets/source_crm/prod_info.csv'
+INTO TABLE bronze_crm_prod_info
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+TRUNCATE TABLE bronze_crm_sales_details;
+
+LOAD DATA LOCAL INFILE '/Users/ananyasingh/Documents/DataEngineering/DataWithBaraa/DataWarehouse/sql-data-warehouse-project-main/datasets/source_crm/sales_details.csv'
+INTO TABLE bronze_crm_sales_details
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+TRUNCATE TABLE bronze_erp_cust_az12;
+
+LOAD DATA LOCAL INFILE '/Users/ananyasingh/Documents/DataEngineering/DataWithBaraa/DataWarehouse/sql-data-warehouse-project-main/datasets/source_erp/CUST_AZ12.csv'
+INTO TABLE bronze_erp_cust_az12
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+TRUNCATE TABLE bronze_erp_loc_a101;
+
+LOAD DATA LOCAL INFILE '/Users/ananyasingh/Documents/DataEngineering/DataWithBaraa/DataWarehouse/sql-data-warehouse-project-main/datasets/source_erp/LOC_A101.csv'
+INTO TABLE bronze_erp_loc_a101
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+TRUNCATE TABLE bronze_erp_px_cat_g1v2;
+
+LOAD DATA LOCAL INFILE '/Users/ananyasingh/Documents/DataEngineering/DataWithBaraa/DataWarehouse/sql-data-warehouse-project-main/datasets/source_erp/PX_CAT_G1V2.csv'
+INTO TABLE bronze_erp_px_cat_g1v2
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
